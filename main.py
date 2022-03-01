090790a948000d6f5d4f541d93b06b705daa92fd
Uros Dolzan <uros.dolzzan@gmail.com>
2022-03-01 21:18:09 +0100

    import tkinter naredil ikono za win in mac

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import platform
+from pathlib import Path
+import tkinter as tk
 from tools.finder import read
 from tools.parser import parse
 from tools.webclient import WebClient
@@ -10,6 +13,24 @@ def main():
     sequences = parse(data)
     WebClient()
     execute(sequences)
+
+    # VIEW
+    root = tk.Tk()
+
+    system = platform.system()
+    if system == 'Windows':
+        root.iconbitmap('./img/ikona.ico')
+    elif system == 'Darwin':
+        # mac
+        img = tk.Image("photo", file="./img/ikona.png")
+        root.iconphoto(True, img) # you may also want to try this.
+        root.tk.call('wm', 'iconphoto', root._w, img)
+
+
+       # root.iconbitmap('./img/ikona.icns')
+    root.title("Ida (v:1.0)")
+    root.geometry('950x400')
+    root.mainloop()
 
 
 if __name__ == '__main__':
